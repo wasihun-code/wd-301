ff51da77dd6098b7bbef01c674ec7388b123ba18
wasihun-code <wasihunageru7947@gmail.com>
2023-10-15 16:11:35 +0530

    Fix missing files

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -4,7 +4,7 @@ import { ThemeContext } from './context/theme';
 import { ProjectsProvider } from './context/projects/context';
 import { MembersProvider } from './context/members/context';
 import React from 'react';
-import router from './routers';
+import router from './routes';
 const App = () => {
   const { theme } = useContext(ThemeContext);
   return (
